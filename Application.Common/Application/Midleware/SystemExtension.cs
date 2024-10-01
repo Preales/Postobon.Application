78dd0e3da77dad3ad22895bd5b484074309316df
Jeison Guerrero <jeitson@gmail.com>
2024-10-01 17:02:52 -0500

    ajustes system extension

---
+++ Application.Common/Application/Midleware/SystemExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Application.Midleware
+{
+    public class SystemExtension //: ISystem
+    {
+       //public List<string> Permissions { get; set; }
+       //public List<RoleInfo> Roles { get; set; }
+       //public Guid TenantId { get; set; }
+       //public UserInfo User { get; set; }
+       //public string Cognito_IdToken { get; set; }
+       //public string Cognito_AccessToken { get; set; }
+        //public void Set(UserInfo user, List<RoleInfo> roles, Guid tenantId, List<string> permissions, string cognito_idToken = null, string cognito_accessToken = null)
+        //{
+            //this.User = user;
+            //this.Roles = roles;
+            //this.TenantId = tenantId;
+            //this.Permissions = permissions;
+
+        //}
+    }
+}
